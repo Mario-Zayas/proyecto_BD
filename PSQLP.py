c094ad1a3faf732bbb19ddaf1534eded02fd297e
Mario-Zayas <123730044+Mario-Zayas@users.noreply.github.com>
2023-03-03 23:13:52 +0100

    Update PSQLP.py

--- PSQLP.py
+++ PSQLP.py
@@ -1,7 +1,6 @@
 
 import psycopg2
 
-
 try:
     conn = psycopg2.connect(user='mario', password='mario', host='localhost', database='database_name')
 except psycopg2.connect.Error as err:
@@ -12,109 +11,115 @@ except psycopg2.connect.Error as err:
 cursor = conn.cursor()
 
 def listar_jugadores():
-    
+
     query = """
             SELECT nombre_entrenador, COUNT(*) AS total_jugadores
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             GROUP BY nombre_entrenador;
             """
 
-    
+
     try:
         cursor.execute(query)
+        for (nombre_entrenador, total_jugadores) in cursor:
+            print(nombre_entrenador, total_jugadores)
     except psycopg2.connect.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
         conn.close()
         exit()
 
-    
-    for (nombre_entrenador, total_jugadores) in cursor:
-        print(nombre_entrenador, total_jugadores)
-
 def buscar_jugadores_entrenador(nombre_entrenador):
-    
+
     query = """
             SELECT id_jugador, nombre_entrenador
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = %s;
             """
 
-    
     try:
         cursor.execute(query, (nombre_entrenador))
+        for (id_jugador, nombre_entrenador) in cursor:
+            print(id_jugador, nombre_entrenador)
     except psycopg2.connect.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
         conn.close()
         exit()
 
-    
-    for (id_jugador, nombre_entrenador) in cursor:
-        print(id_jugador, nombre_entrenador)
 
 def mostrar_entrenador_jugadores():
-    
+
     nombre_entrenador = input("Introduce el nombre del entrenador: ")
 
-    
+
     query = """
             SELECT *
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = %s;
             """
 
-    
+
     try:
         cursor.execute(query, (nombre_entrenador))
+        for (nlicencia, nombre_entrenador, email, id_jugador, posicion_ant_camp, coef_elo, altura) in cursor:
+            print("Entrenador: ", nlicencia, nombre_entrenador, email)
+            print("Jugador: ", id_jugador, posicion_ant_camp, coef_elo, altura)
     except psycopg2.connect.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
         conn.close()
         exit()
 
-    
-    for (nlicencia, nombre_entrenador, email, id_jugador, posicion_ant_camp, coef_elo, altura) in cursor:
-        print("Entrenador: ", nlicencia, nombre_entrenador, email)
-        print("Jugador: ", id_jugador, posicion_ant_camp, coef_elo, altura)
-
-def insertar_jugador(id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura):
-    
+        
+def insertar_jugador(jugadores):
     query = """
-            INSERT INTO Jugador (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
-            VALUES (%s, %s, %s, %s, %s);
-            """
-
-    
+        INSERT INTO Jugador (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
+        VALUES (%s, %s, %s, %s, %s);
+    """
     try:
-        cursor.execute(query, (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura))
+        cursor = conn.cursor()
+        for jugador in jugadores:
+            cursor.execute(query, jugador)
+        conn.commit()
+        cursor.close()
+        print(f"Se han insertado {len(jugadores)} jugadores.")
     except psycopg2.connect.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
         conn.close()
         exit()
+
+
+    
     
     conn.commit()
 
 def eliminar_jugadores_entrenador(nombre_entrenador):
-    
     query = """
         DELETE FROM Jugador
         WHERE nlicencia IN (
-        SELECT nlicencia FROM Entrenador WHERE nombre_entrenador = %s);
-        """
+            SELECT nlicencia
+            FROM Entrenador
+            WHERE nombre_entrenador = %s
+        );
+    """
     try:
-        cursor.execute(query, (nombre_entrenador))
+        cursor = conn.cursor()
+        cursor.execute(query, (nombre_entrenador,))
+        num_jugadores_eliminados = cursor.rowcount
+        conn.commit()
+        cursor.close()
+        print(f"Se han eliminado {num_jugadores_eliminados} jugadores del entrenador {nombre_entrenador}.")
     except psycopg2.connect.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
         conn.close()
         exit()
+
     
     conn.commit()
-    
-    print(cursor.rowcount, "jugadores eliminados")
-    
+
 def actualizar_jugador(id_jugador, altura, coef_elo):
     
     query = """
@@ -129,9 +134,6 @@ def actualizar_jugador(id_jugador, altura, coef_elo):
         cursor.close()
         conn.close()
         exit()
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 
 def menu():
