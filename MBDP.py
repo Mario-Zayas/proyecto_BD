9c9225a93582d96f0ee0d3743f61f6c610b62e9e
Mario-Zayas <123730044+Mario-Zayas@users.noreply.github.com>
2023-03-03 22:45:53 +0100

    Add files via upload

---
+++ MBDP.py
@@ -0,0 +1,151 @@
+import mysql.connector
+
+
+try:
+    cnx = mysql.connector.connect(user='mario', password='mario', host='localhost', database='database_name')
+except mysql.connector.Error as err:
+    print(f"Error al conectarse a la base de datos: {err}")
+    exit()
+
+cursor = cnx.cursor()
+
+def listar_jugadores():
+    
+    query = """
+            SELECT nombre_entrenador, COUNT(*) AS total_jugadores
+            FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
+            GROUP BY nombre_entrenador;
+            """
+
+    
+    try:
+        cursor.execute(query)
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()
+
+   
+    for (nombre_entrenador, total_jugadores) in cursor:
+        print(nombre_entrenador, total_jugadores)
+
+def buscar_jugadores_entrenador(nombre_entrenador):
+    
+    query = """
+            SELECT id_jugador, nombre_entrenador
+            FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
+            WHERE nombre_entrenador = %s;
+            """
+
+    
+    try:
+        cursor.execute(query, (nombre_entrenador,))
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()
+    
+    for (id_jugador, nombre_entrenador) in cursor:
+        print(id_jugador, nombre_entrenador)
+
+def mostrar_entrenador_jugadores():
+    
+    nombre_entrenador = input("Introduce el nombre del entrenador: ")
+
+    
+    query = """
+            SELECT *
+            FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
+            WHERE nombre_entrenador = %s;
+            """
+
+    
+    try:
+        cursor.execute(query, (nombre_entrenador,))
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()
+
+    
+    for (nlicencia, nombre_entrenador, email, id_jugador, posicion_ant_camp, coef_elo, altura) in cursor:
+        print("Entrenador: ", nlicencia, nombre_entrenador, email)
+        print("Jugador: ", id_jugador, posicion_ant_camp, coef_elo, altura)
+
+def insertar_jugador(id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura):
+    
+    query = """
+            INSERT INTO Jugador (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
+            VALUES (%s, %s, %s, %s, %s);
+            """
+
+    
+    try:
+        cursor.execute(query, id_jugador=id_jugador, nlicencia=nlicencia, posicion_ant_camp=posicion_ant_camp, coef_elo=coef_elo, altura=altura)
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()  
+
+    
+    
+    cnx.commit()
+
+def eliminar_jugadores_entrenador(nombre_entrenador):
+    
+    query = """
+            DELETE FROM Jugador
+            WHERE nlicencia = (
+            SELECT nlicencia
+            FROM Entrenador
+            WHERE nombre_entrenador = %s);
+            """
+    try:
+        cursor.execute(query, (nombre_entrenador,))
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()
+    
+    cnx.commit()
+
+def actualizar_jugador(id_jugador, altura, coef_elo):
+    
+    query = """
+            UPDATE Jugador
+            SET altura= %s, coef_elo = %s 
+            WHERE id_jugador = %s;
+            """
+    try:
+        cursor.execute(query, (altura, coef_elo, id_jugador))
+    except mysql.connector.Error as err:
+        print(f"Error al ejecutar la consulta: {err}")
+        cursor.close()
+        cnx.close()
+        exit()
+    
+    cnx.commit()
+
+    cursor.close()
+    cnx.close()
+    
+def menu():
+    print("-------------------------------------------")
+    print("Proyecto de BBDD")
+    print("\n")
+    print("1. Mostrar la cantidad de jugadores que hay en la lista participantes y lista los jugadores")
+    print("2. Buscar jugadores cuyo entrenador tenga por nombre Manolo")
+    print("3. Pide por teclado un entrenador y muestra sus datos y los jugadores a los que entrena")
+    print("4. Inserta en la tabla Jugador un nuevo jugador")
+    print("5. Elimina a los jugadores cuyo entrenador tenga por nombre Agosto")
+    print("6. Actualizar la altura y coeficiente elo de un jugador que ya ha sido registrado")
+    print("7. Salir")
+    print("\n")
+    print("-------------------------------------------")
+    opcion=int(input("Seleccione una de las opciones: "))
+    return opcion
---
+++ menu3.py
@@ -0,0 +1,30 @@
+from PSQLP import *
+opcion = menu()
+while opcion != 7:
+    if opcion == 1:
+        listar_jugadores()
+    elif opcion == 2:
+        nombre_entrenador = input("Ingrese el nombre del entrenador: ")
+        buscar_jugadores_entrenador(nombre_entrenador)
+    elif opcion == 3:
+        mostrar_entrenador_jugadores()
+    elif opcion == 4:
+        id_jugador = input("Ingrese el ID del jugador: ")
+        nlicencia = input("Ingrese la licencia del entrenador: ")
+        posicion_ant_camp = input("Ingrese la posición del jugador en el campeonato anterior: ")
+        coef_elo = input("Ingrese el coeficiente ELO del jugador: ")
+        altura = input("Ingrese la altura del jugador: ")
+        insertar_jugador(id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
+    elif opcion == 5:
+        nombre_entrenador = input("Ingrese el nombre del entrenador: ")
+        eliminar_jugadores_entrenador(nombre_entrenador)
+    elif opcion == 6:
+        id_jugador = input("Ingrese el ID del jugador: ")
+        nueva_posicion = input("Ingrese la nueva posición del jugador: ")
+        nueva_altura = input("Ingrese la nueva altura del jugador: ")
+        actualizar_jugador(id_jugador, nueva_posicion, nueva_altura)
+    elif opcion == 7:
+        print("")
+    else:
+        print("Opción no válida, utilice una opción válida.")
+    opcion = menu()
