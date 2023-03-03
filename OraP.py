ba0192e697f3b1e51bed78934b513beae2e5bdd6
Mario-Zayas <123730044+Mario-Zayas@users.noreply.github.com>
2023-03-03 22:50:24 +0100

    Update OraP.py

--- OraP.py
+++ OraP.py
@@ -7,11 +7,11 @@ except cx_Oracle.connect.Error as err:
         print(f"Error al conectarse a la base de datos: {err}")
         exit()
 
-# Creamos un cursor para ejecutar las consultas SQL
+
 cursor = conn.cursor()
 
 def listar_jugadores():
-    # Consulta para obtener la lista de jugadores y el total de jugadores
+    
     query = """
             SELECT nombre_entrenador, COUNT(*) AS total_jugadores
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
@@ -27,19 +27,19 @@ def listar_jugadores():
         exit()
 
 
-    # Recuperamos los resultados
+    
     for (nombre_entrenador, total_jugadores) in cursor:
         print(nombre_entrenador, total_jugadores)
 
 def buscar_jugadores_entrenador(nombre_entrenador):
-    # Consulta para buscar jugadores cuyo entrenador tenga por nombre "Manolo"
+    
     query = """
             SELECT id_jugador, nombre_entrenador
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = :nombre_entrenador
             """
 
-    # Ejecutamos la consulta con el nombre de entrenador proporcionado
+    
     try:
         cursor.execute(query, nombre_entrenador=nombre_entrenador)
     except cx_Oracle.connect.Error as err:
@@ -48,22 +48,22 @@ def buscar_jugadores_entrenador(nombre_entrenador):
         conn.close()
         exit()
 
-    # Recuperamos los resultados
+   
     for (id_jugador, nombre_entrenador) in cursor:
         print(id_jugador, nombre_entrenador)
 
 def mostrar_entrenador_jugadores():
-    # Pedimos el nombre del entrenador
+    
     nombre_entrenador = input("Introduce el nombre del entrenador: ")
 
-    # Consulta para obtener los datos del entrenador y los jugadores que entrena
+    
     query = """
             SELECT *
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = :nombre_entrenador
             """
 
-    # Ejecutamos la consulta con el nombre de entrenador proporcionado
+   
     try:
         cursor.execute(query, nombre_entrenador=nombre_entrenador)
     except cx_Oracle.connect.Error as err:
@@ -72,19 +72,19 @@ def mostrar_entrenador_jugadores():
         conn.close()
         exit()
 
-    # Recuperamos los resultados
+   
     for (nlicencia, nombre_entrenador, email, id_jugador, posicion_ant_camp, coef_elo, altura) in cursor:
         print("Entrenador: ", nlicencia, nombre_entrenador, email)
         print("Jugador: ", id_jugador, posicion_ant_camp, coef_elo, altura)
 
 def insertar_jugador(id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura):
-    # Consulta para insertar un nuevo jugador en la tabla Jugador
+    
     query = """
             INSERT INTO Jugador (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
             VALUES (:id_jugador, :nlicencia, :posicion_ant_camp, :coef_elo, :altura)
             """
 
-    # Ejecutamos la consulta con los datos del nuevo jugador
+    
     cursor.execute(query, id_jugador=id_jugador, nlicencia=nlicencia, posicion_ant_camp=posicion_ant_camp, coef_elo=coef_elo, altura=altura)
     try:
         cursor.execute(query, id_jugador=id_jugador, nlicencia=nlicencia, posicion_ant_camp=posicion_ant_camp, coef_elo=coef_elo, altura=altura)
@@ -93,11 +93,11 @@ def insertar_jugador(id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
         cursor.close()
         conn.close()
         exit()
-    # Hacemos commit para guardar los cambios en la base de datos
+    
     conn.commit()
 
 def eliminar_jugadores_entrenador(nombre_entrenador):
-   # Consulta para eliminar los jugadores cuyo entrenador tenga por nombre "Agosto"
+  
    query = """
       DELETE FROM Jugador
       WHERE nlicencia IN (SELECT nlicencia FROM Entrenador WHERE nombre_entrenador = :nombre_entrenador)
@@ -110,11 +110,11 @@ def eliminar_jugadores_entrenador(nombre_entrenador):
         conn.close()
         exit()
 
-   # Hacemos commit para guardar los cambios en la base de datos
+   
    conn.commit()
    
 def actualizar_jugador(id_jugador, altura, coef_elo):
-    # Consulta para actualizar la altura y el coeficiente elo de un jugador existente en la tabla Jugador
+    
     query = """
             UPDATE Jugador
             SET altura= %s, coef_elo = %s 
