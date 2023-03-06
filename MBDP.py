c34cc0396c22e73e33223144c1398d16ca3f588b
Mario-Zayas <123730044+Mario-Zayas@users.noreply.github.com>
2023-03-06 14:24:07 +0100

    Update MBDP.py

--- MBDP.py
+++ MBDP.py
@@ -1,67 +1,70 @@
 import mysql.connector
 
 
-try:
-    cnx = mysql.connector.connect(user='mario', password='mario', host='localhost', database='database_name')
-except mysql.connector.Error as err:
-    print(f"Error al conectarse a la base de datos: {err}")
-    exit()
+def Conectar_BD(host, usuario, password, database):
+    try:
+        db = mysql.connector.connect(
+            host=host,
+            user=usuario,
+            password=password,
+            database=database
+        )
 
-cursor = cnx.cursor()
+        return db
+    except mysql.connector.Error as e:
+        print("No puedo conectar a la base de datos:", e)
+        return None
 
-def listar_jugadores():
+
+
+
+def listar_jugadores(db):
+    cursor = db.cursor()
     
-    query = """
+    try:
+        query = """
             SELECT nombre_entrenador, COUNT(*) AS total_jugadores
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             GROUP BY nombre_entrenador;
             """
-
-    
-    try:
         cursor.execute(query)
         for (nombre_entrenador, total_jugadores) in cursor:
             print(nombre_entrenador, total_jugadores)
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
         exit()
 
 
-def buscar_jugadores_entrenador(nombre_entrenador):
+def buscar_jugadores_entrenador(nombre_entrenador, db):
+    cursor = db.cursor()
     
-    query = """
+    try:
+        query = """
             SELECT id_jugador, nombre_entrenador
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = %s;
             """
-
-    
-    try:
         cursor.execute(query, (nombre_entrenador,))
         for (id_jugador, nombre_entrenador) in cursor:
             print(id_jugador, nombre_entrenador)
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
+        db.close()
         exit()
     
 
-def mostrar_entrenador_jugadores():
-    
+def mostrar_entrenador_jugadores(db):
+    cursor = db.cursor()
     nombre_entrenador = input("Introduce el nombre del entrenador: ")
-
     
-    query = """
+    try:
+        query = """
             SELECT *
             FROM Entrenador JOIN Jugador ON Entrenador.nlicencia = Jugador.nlicencia
             WHERE nombre_entrenador = %s;
             """
-
-    
-    try:
         cursor.execute(query, (nombre_entrenador,))
         for (nlicencia, nombre_entrenador, email, id_jugador, posicion_ant_camp, coef_elo, altura) in cursor:
             print("Entrenador: ", nlicencia, nombre_entrenador, email)
@@ -69,78 +72,81 @@ def mostrar_entrenador_jugadores():
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
+        db.close()
         exit()
 
     
 
-def insertar_jugador(jugadores):
-    query = """
+def insertar_jugador(jugadores,db):
+    
+    cursor = db.cursor()
+    try:
+        query = """
         INSERT INTO Jugador (id_jugador, nlicencia, posicion_ant_camp, coef_elo, altura)
         VALUES (%s, %s, %s, %s, %s);
     """
-    try:
-        cursor = cnx.cursor()
+        cursor = db.cursor()
         for jugador in jugadores:
             cursor.execute(query, jugador)
-        cnx.commit()
+        db.commit()
         cursor.close()
         print(f"Se han insertado {len(jugadores)} jugadores.")
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
+        db.close()
         exit()
 
 
     
     
-    cnx.commit()
+    db.commit()
 
-def eliminar_jugadores_entrenador(nombre_entrenador):
-    query = """
+def eliminar_jugadores_entrenador(nombre_entrenador,db):
+    cursor=db.cursor()
+    try:
+        query = """
         DELETE FROM Jugador
         WHERE nlicencia IN (
             SELECT nlicencia
             FROM Entrenador
             WHERE nombre_entrenador = %s
         );
-    """
-    try:
-        cursor = cnx.cursor()
+            """
+        cursor = db.cursor()
         cursor.execute(query, (nombre_entrenador,))
         num_jugadores_eliminados = cursor.rowcount
-        cnx.commit()
+        db.commit()
         cursor.close()
         print(f"Se han eliminado {num_jugadores_eliminados} jugadores del entrenador {nombre_entrenador}.")
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
+        db.close()
         exit()
 
     
-    cnx.commit()
+    db.commit()
 
-def actualizar_jugador(id_jugador, altura, coef_elo):
-    
-    query = """
+def actualizar_jugador(id_jugador, altura, coef_elo,db):
+    cursor = db.cursor()
+    try:
+        query = """
             UPDATE Jugador
             SET altura= %s, coef_elo = %s 
             WHERE id_jugador = %s;
             """
-    try:
         cursor.execute(query, (altura, coef_elo, id_jugador))
     except mysql.connector.Error as err:
         print(f"Error al ejecutar la consulta: {err}")
         cursor.close()
-        cnx.close()
+        db.close()
         exit()
     
-    cnx.commit()
+    db.commit()
 
     cursor.close()
-    cnx.close()
+    db.close()
     
 def menu():
     print("-------------------------------------------")
